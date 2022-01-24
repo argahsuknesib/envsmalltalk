470385c3ed2af70d664af163c0eaf6925ecac56d
argahsuknesib <k.bisen@yahoo.com>
2022-01-24 15:03:05 +0100

    first commit

---
+++ src/main/java/cps2/emse/fr/envsmalltalk/EnvsmalltalkApplication.java
@@ -0,0 +1,13 @@
+package cps2.emse.fr.envsmalltalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvsmalltalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvsmalltalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cps2/emse/fr/envsmalltalk/EnvsmalltalkApplicationTests.java
@@ -0,0 +1,13 @@
+package cps2.emse.fr.envsmalltalk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvsmalltalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
